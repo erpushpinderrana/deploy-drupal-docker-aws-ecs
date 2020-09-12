f3e13d6ad153a03e02b22f44d53dbcc81a71c5aa
Pushpinder Rana <pushpinder.rana@publicissapient.com>
2020-09-12 11:40:10 -0400

    Added aws jenkins pipeline script and pushing aws image to ECR script

---
+++ aws_scripts/service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SERVICE_NAME="aws-ecs-service"
+IMAGE_VERSION="v_"${BUILD_NUMBER}
+TASK_FAMILY="aws-apache-php-def"
+
+# Create a new task definition for this build
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" aws-apache-php-def.json > aws-apache-php-def-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family aws-apache-php-def.json --cli-input-json file://aws-apache-php-def-v_${BUILD_NUMBER}.json
+
+# Update the service with the new task definition and desired count
+TASK_REVISION=`aws ecs describe-task-definition --task-definition aws-apache-php-def | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
+if [ "$DESIRED_COUNT" = "0" ]; then
+    DESIRED_COUNT="1"
+fi
+
+aws ecs update-service --force-new-deployment --cluster aws-drupal-ecs-cluster --region us-east-2 --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
